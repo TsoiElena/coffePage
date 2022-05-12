87dd177f41e0589e993f16a99cadb3a123f1cd9f
Elena Tsoi <tsoielena66@gmail.com>
2022-05-12 18:47:22 +0300

    final version

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,16 @@
 import React, {useEffect, useState} from 'react'
-import Preloader from '../Preloader'
-import CoffeePage from '../CoffeePage'
+import Preloader from './Preloader'
+import CoffeePage from './CoffeePage'
 
 const  Main = () => {
-    const [loading, setLoading] = useState(true)
+    const successCode = 200
+    const [isLoading, setLoading] = useState(true)
     const [coffeeList, setCoffeeList] = useState([])
 
     useEffect(() => {
         fetch('https://random-data-api.com/api/coffee/random_coffee?size=20')
             .then((res) => {
-                if (res.status >= 200 && res.status < 300) {
+                if (res.status === successCode) {
                     return res
                 } else {
                     let error = new Error(res.statusText)
@@ -29,19 +30,14 @@ const  Main = () => {
                 setLoading(false)
             })
     }, [])
-    return (
-        <>
-            { loading ? <Preloader/> : (
-                <>
-                    {
-                        coffeeList.length === 0 ? <h1>Результатов не найдено</h1> : (
-                            <CoffeePage coffeeList={coffeeList}/>
-                        )
-                    }
-                </>
-            )}
-        </>
-    )
+
+    if (isLoading) {
+        return <Preloader/>
+    }
+    if (coffeeList.length === 0) {
+        return <h1>Результатов не найдено</h1>
+    }
+    return <CoffeePage coffeeList={coffeeList}/>
 }
 
 export default Main
